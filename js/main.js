3bd88cd58a3c7901a4151fa28aa19e83003795f8
mimiojisan <89684125+mimiojisan@users.noreply.github.com>
2022-03-11 20:18:43 +0900

    second initial

--- js/main.js
+++ js/main.js
@@ -28,3 +28,4 @@ navLink.forEach(function(link){
         header.classList.remove("header");
     });
 });
+
--- js/theme.js
+++ js/theme.js
@@ -7,7 +7,7 @@ const $break = 960;
 
 if($size > $break) {
 function FixedAnime() {
-	var elemTop = $('#about').offset().top;
+	var elemTop = $('#works').offset().top;
 	var scroll = $(window).scrollTop();
 
 	if(scroll <= 20){
@@ -30,3 +30,23 @@ $(window).scroll(function () {
 	FixedAnime();
 });
 }
+
+  // 各コンテンツをふわっと表示させるJS 
+window.onload = function() {
+	scroll_effect();
+  
+	$(window).scroll(function(){
+	  scroll_effect();
+	});
+  
+	function scroll_effect(){
+	  $('.effect-fade').each(function(){
+		var elemPos = $(this).offset().top;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll > elemPos - windowHeight){
+		  $(this).addClass('effect-scroll');
+		}
+	  });
+	}
+  };
